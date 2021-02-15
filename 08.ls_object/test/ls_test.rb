476fb8ac4fc46912e6532efd3846a3035ce3dfbe
ksmxxxxxx <kankichi69@gmail.com>
2021-02-15 21:40:56 +0900

    :pencil2: Fix class name and variable name

--- 08.ls_object/test/ls_test.rb
+++ 08.ls_object/test/ls_test.rb
@@ -3,7 +3,7 @@
 require 'minitest/autorun'
 require './lib/filedata'
 require './lib/filelist'
-require './lib/show'
+require './lib/display'
 
 class LsTest < Minitest::Test
   TARGET_PATH = 'test/fixtures'
@@ -15,29 +15,29 @@ class LsTest < Minitest::Test
       dir_foobar          dummy_file_002.txt  dummy_file_004.txt  lorem_ipsum_10p.txt lorem_ipsum_2p.txt  symlink_file.txt
       dummy_file_001.txt  dummy_file_003.txt  dummy_file_005.txt  lorem_ipsum_1p.txt  lorem_ipsum_5p.txt
     TEXT
-    show = Show.new(TARGET_PATH)
-    show.list_without_dotfile
-    assert_equal expected, show.short_format_split_into_columns
+    display = Display.new(TARGET_PATH)
+    display.list_without_dotfile
+    assert_equal expected, display.short_format_split_into_columns
   end
 
   def test_ls_short_format
     expected = <<~TEXT.chomp
       fixtures   ls_test.rb
     TEXT
-    show = Show.new(TARGET_PATH_NO_SPRIT)
-    show.list_without_dotfile
-    assert_equal expected, show.short_format
+    display = Display.new(TARGET_PATH_NO_SPRIT)
+    display.list_without_dotfile
+    assert_equal expected, display.short_format
   end
 
-  def test_ls_show_dotfiles
+  def test_ls_display_dotfiles
     skip '環境によって表示が変わるので、lsコマンドの実行結果をexpectedとしてテストしたかったが、やり方が見つけられなかったのでこのテストはSkipします'
     expected = <<~TEXT.chomp
       .                   dir_foobar          dummy_file_002.txt  dummy_file_004.txt  lorem_ipsum_10p.txt lorem_ipsum_2p.txt  symlink_file.txt
       ..                  dummy_file_001.txt  dummy_file_003.txt  dummy_file_005.txt  lorem_ipsum_1p.txt  lorem_ipsum_5p.txt
     TEXT
-    show = Show.new(TARGET_PATH)
-    show.list_contain_dotfile
-    assert_equal expected, show.short_format_split_into_columns
+    display = Display.new(TARGET_PATH)
+    display.list_contain_dotfile
+    assert_equal expected, display.short_format_split_into_columns
   end
 
   def test_ls_reverse
@@ -46,26 +46,26 @@ class LsTest < Minitest::Test
       symlink_file.txt    lorem_ipsum_2p.txt  lorem_ipsum_10p.txt dummy_file_004.txt  dummy_file_002.txt  dir_foobar
       lorem_ipsum_5p.txt  lorem_ipsum_1p.txt  dummy_file_005.txt  dummy_file_003.txt  dummy_file_001.txt
     TEXT
-    show = Show.new(TARGET_PATH)
-    show.list_without_dotfile
-    show.list_reverse
-    assert_equal expected, show.short_format_split_into_columns
+    display = Display.new(TARGET_PATH)
+    display.list_without_dotfile
+    display.list_reverse
+    assert_equal expected, display.short_format_split_into_columns
   end
 
   def test_ls_long_format
     expected = `ls -l #{TARGET_PATH}`.chomp
-    show = Show.new(TARGET_PATH)
-    show.list_without_dotfile
-    show.list_file_stat
-    assert_equal expected, show.long_format
+    display = Display.new(TARGET_PATH)
+    display.list_without_dotfile
+    display.list_file_stat
+    assert_equal expected, display.long_format
   end
 
   def test_ls_all_options
     expected = `ls -arl #{TARGET_PATH}`.chomp
-    show = Show.new(TARGET_PATH)
-    show.list_contain_dotfile
-    show.list_reverse
-    show.list_file_stat
-    assert_equal expected, show.long_format
+    display = Display.new(TARGET_PATH)
+    display.list_contain_dotfile
+    display.list_reverse
+    display.list_file_stat
+    assert_equal expected, display.long_format
   end
 end
