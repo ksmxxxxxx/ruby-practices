123859bfab1eda59d320c73c1d392dfa768b7731
ksmxxxxxx <kankichi69@gmail.com>
2021-02-17 11:39:19 +0900

    :put_litter_in_its_place: Remove the require path
    The files was not used directly

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -1,12 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'etc'
 require 'optparse'
-require 'io/console/size'
 
-require_relative 'filedata'
-require_relative 'filelist'
 require_relative 'display'
 
 class Ls
